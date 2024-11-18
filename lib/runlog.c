dde1f3c204b68861db43b72b3a38e615f7aa3807
ejudge user <ejudge@ejudge-test.ru-central1.internal>
2024-11-18 13:37:23 +0000

    Fix

--- lib/runlog.c
+++ lib/runlog.c
@@ -954,10 +954,6 @@ run_get_attempts(
     ASSERT(re->user_id == sample_re->user_id);
     if (i >= runid) break;
 
-    if (group_merge_flag > 0 && re->group_scores) {
-      group_scores_merge_1(p_group_count, p_group_scores, run_get_group_scores(state, re->group_scores));
-    }
-
     if (re->status == RUN_VIRTUAL_START || re->status == RUN_VIRTUAL_STOP) continue;
     if (re->prob_id != sample_re->prob_id) continue;
     if ((re->status == RUN_COMPILE_ERR) && skip_ce_flag) continue;
